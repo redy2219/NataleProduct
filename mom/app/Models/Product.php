344d00ba49b37b97c2a7f197702c69ff4501249d
rodionova_k <rodionova@enterego.ru>
2021-03-10 01:08:41 +0300

    correct model

--- mom/app/Models/Product.php
+++ mom/app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected array $table = [
+        (string)'id',
+        (string)'name',
+        (string)'description',
+        (int)'price',
+
+    ];
 }
